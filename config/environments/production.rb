60b283577584ba4427f1ed9178110d5b1dc59aa5
bartezic <bartezic@gmail.com>
2013-10-17 13:08:52 +0300

    setup mail server

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,12 @@ Wed::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { :host => "146.185.151.232" }
+  # ActionMailer Config
+  # Setup for production - deliveries, no errors raised
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
