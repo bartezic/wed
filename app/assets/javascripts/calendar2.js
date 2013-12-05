037e6b87b57c4a615864c7569cdcb430d3a901fd
bartezic <bartezic@inbox.ru>
2013-12-05 11:10:05 +0200

    works with calendar

--- app/assets/javascripts/calendar2.js
+++ app/assets/javascripts/calendar2.js
@@ -26,7 +26,7 @@ window.WedCity.calendar = {
   getDays: function(date) {
     var date, days = '<w>', i, counter = 0;
 
-    for(i = 1; i <= this.daysBefore(date); i++){
+    for (i = 1; i <= this.daysBefore(date); i++) {
       days += "<e></e>";
       counter += 1;
     };
@@ -43,29 +43,33 @@ window.WedCity.calendar = {
   },
 
   generateMonth: function(month, year){
-    var date;
+    var date = new Date(year,month,1);
 
-    if (month && year) {
-      date = new Date(year,month,1);
-    } else {
-      date = new Date();
-    };
-
-    return  "<div class='month'>\
+    return  "<div class='month' data-month='" + month + "' data-year='" + year + "'>\
               <div class='header'>" + window.WedCity.l.month.name[date.getMonth()] + "    " + date.getFullYear() + "</div>\
-              <div class='body'>\
-                <div class='days'>" + this.getDays(date); + "</div>\
-              </div>\
+              <div class='days'>" + this.getDays(date); + "</div>\
             </div>";
   },
 
   createMonths: function() {
-    var self = this,
-        month = self.generateMonth();
+    var self = this, prevWidth, i,
+        date = new Date(),
+        month = date.getMonth(),
+        year = date.getFullYear();
 
-        console.log(month)
+    self.configs.mSize = Math.floor(($('.container').width()-160)/220);
     
-    self.elems.monthsWrap.append(month);
+    for (i = 0; i < self.configs.mSize; i++) {
+      self.elems.months.append($(self.generateMonth(month, year)));
+      if (month === 11) {
+        month = 0;
+        year++;
+      } else {
+        month++;
+      };
+    };
+
+    self.elems.viewport.width(self.configs.mSize*220);
   },
 
   createElems: function(){
@@ -79,9 +83,9 @@ window.WedCity.calendar = {
         weekDays = $("<div class='day_names left'>" + self.weekDays() + "</div>\
                     <div class='day_names right'>" + self.weekDays() + "</div>"),
         viewport = $("<div class='viewport'></div>"),
-        monthsWrap = $("<div class='months'></div>");
+        months = $("<div class='months'></div>");
 
-    viewport.append(monthsWrap);
+    viewport.append(months);
     self.elems.cont.append([navLeft, navRight, weekDays, viewport]);
 
     $.extend(self.elems, {
@@ -89,12 +93,21 @@ window.WedCity.calendar = {
       navRight: navRight,
       weekDays: weekDays,
       viewport: viewport,
-      monthsWrap: monthsWrap
+      months: months
     });
+  },
+
+  resizeCalendar: function() {
+    var self = this,
+        mSize = Math.floor(($('.container').width()-160)/220);
   },
   
   initHandlers: function (){
+    var self = this;
 
+    $(window).resize(function() {
+      self.resizeCalendar();
+    });
   },
 
   init: function(){
