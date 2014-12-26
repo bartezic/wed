61a9f5401a915b77a2ff1243d03c7b143ba3550e
Viktor Oleksyn <bartezic@gmail.com>
2014-12-26 14:38:05 +0200

    slider changes

--- app/controllers/app/static_controller.rb
+++ app/controllers/app/static_controller.rb
@@ -2,7 +2,7 @@ module App
   class StaticController < BaseController
     def home
       @last_partners = Partner.active.order('created_at DESC').limit(10)
-      @slider_ads = SliderAd.where(active: true)
+      @slider_ads = SliderAd.where(active: true).order('random()')
     end
 
     def empty
