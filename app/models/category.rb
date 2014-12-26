85904e6c0a56bfc538313956f087ad6237e6ca52
Viktor Oleksyn <bartezic@gmail.com>
2014-12-26 11:02:53 +0200

    change paperclip convert options

--- app/models/category.rb
+++ app/models/category.rb
@@ -8,23 +8,27 @@ class Category < ActiveRecord::Base
   has_many :partners, through: :involvings
 
   has_attached_file :logo,
-    styles: {
-      thumb: {
-        geometry: '265x135#',
-        quality: 100,
-        format: 'JPG'
-      }
-    },
+    styles: { thumb: '265x135#' },
+    convert_options: { thumb: '-quality 100' },
     default_url: "/assets/ph/category_:attachment_:style.gif"
 
   validates_attachment_content_type :logo, :content_type => /\Aimage\/.*\Z/
 
-  before_validation :upload_logo_from_remote_url
+  attr_reader :logo_remote_url
+  # before_validation :upload_logo_from_remote_url
   # before_save :change_file_name
 
-  def upload_logo_from_remote_url
-    self.logo = URI.parse(logo_remote_url) if logo_remote_url.present?
-  rescue
+  # def upload_logo_from_remote_url
+  #   self.logo = URI.parse(logo_remote_url) if logo_remote_url.present?
+  # rescue
+  # end
+
+  def logo_remote_url=(url_value)
+    self.logo = URI.parse(url_value)
+    # Assuming url_value is http://example.com/photos/face.png
+    # avatar_file_name == "face.png"
+    # avatar_content_type == "image/png"
+    @logo_remote_url = url_value
   end
 
   def normalize_friendly_id(input)
