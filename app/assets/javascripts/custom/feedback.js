cdfb37c8478eb2d8bb544cdc802d04610d9de35e
Viktor Oleksyn <bartezic@gmail.com>
2014-12-29 02:26:58 +0200

    few fixes for feedbacks

--- app/assets/javascripts/custom/feedback.js
+++ app/assets/javascripts/custom/feedback.js
@@ -3,12 +3,14 @@ window.WedCity = window.WedCity || {};
 window.WedCity.feedback = {
   initHandlers: function(){
     $('#feedback').on('show.bs.modal', function (event) {
-      var button = $(event.relatedTarget);
-      var name = button.data('name');
-      var email = button.data('email');
-      var modal = $(this);
-      modal.find('form input#feedback_name').val(name);
-      modal.find('form input#feedback_email').val(email);
+      var button = $(event.relatedTarget),
+          name = button.data('name'),
+          email = button.data('email'),
+          modal = $(this),
+          name_el = modal.find('form input#feedback_name'),
+          email_el = modal.find('form input#feedback_email');
+      if(name_el.val() == ''){ name_el.val(name) };
+      if(email_el.val() == ''){ email_el.val(email) };
     })
   },
 
